33dc2b9437d285bc780589ab4be0a8ae60d0099a
Michael Turkington <m_turkington@hotmail.co.uk>
2024-03-29 15:02:05 +0000

    Test

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/camera-kit";
+
+(async function main() {
+    const apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzExNjAxNjAyLCJzdWIiOiIyY2ZkMzFiZC1lMWFmLTQ1N2QtYWJmYy1hOThhNzkzMzRlMWZ-U1RBR0lOR34wOTM2YTE4NC00NjJiLTQ5YjgtOGZjYi05YzAwMDNmMmUwNzUifQ.xEpM3aiqXNU6uIM6w70BfsWag42WdXt3wnGv-hckOUk";
+    const cameraKit = await bootstrapCameraKit({ apiToken });
+
+    const canvas = document.getElementById("my-canvas");
+    const liveRenderTarget = document.getElementById(
+         'my-canvas'
+          ) as HTMLCanvasElement;
+    const session = await cameraKit.createSession({ liveRenderTarget});
+    session.events.addEventListener('error', (event) => {
+      if (event.detail.error.name === 'LensExecutionError') {
+        console.log('The current Lens encountered an error and was removed.', event.detail.error);
+      }
+    });
+
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const source = createMediaStreamSource(stream, { transform: Transform2D.MirrorX });
+    await session.setSource(source);
+    await source.setRenderSize(680, 480);
+
+    const lens = await cameraKit.lensRepository.loadLens("50507980875", "663f5bb4-e694-4260-862f-8979394d866a");
+    await session.applyLens(lens);
+
+    await session.play();
+    console.log("Lens rendering has started!");
+})();
+
+//main();
